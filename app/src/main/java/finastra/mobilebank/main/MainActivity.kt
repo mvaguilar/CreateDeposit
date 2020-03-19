1ef3fdae2a6e2fac8982c5e363a72bb55349920e
martinvincentaguilar <mvaguilar616@gmail.com>
2020-03-19 14:03:25 +0800

    added Kotlin MPP library

--- app/src/main/java/finastra/mobilebank/main/MainActivity.kt
+++ app/src/main/java/finastra/mobilebank/main/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.VERTICAL
+import com.example.deh_kotlin_mpp.*
 import finastra.mobilebank.createdeposit.CreateDepositActivity
 import finastra.mobilebank.createdeposit.R
 import finastra.mobilebank.createdeposit.model.DepositProduct
@@ -24,8 +25,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         login_button.setOnClickListener {
-            val intent = Intent(this, CreateDepositActivity::class.java)
-            startActivity(intent)
+            if (username_edit_text.text!!.isNotEmpty() && password_edit_text.text!!.isNotEmpty()) {
+                Toast.makeText(this, "MPP: "+onLoginSuccess(),Toast.LENGTH_LONG).show()
+                val intent = Intent(this, CreateDepositActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "MPP: "+onLoginFail(),Toast.LENGTH_LONG).show()
+            }
+        }
+
+        create_button.setOnClickListener {
+            if (username_edit_text.text!!.isNotEmpty() && password_edit_text.text!!.isNotEmpty()) {
+                Toast.makeText(this, "MPP: " + onRegSuccess(), Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "MPP: " + onRegFail(), Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
